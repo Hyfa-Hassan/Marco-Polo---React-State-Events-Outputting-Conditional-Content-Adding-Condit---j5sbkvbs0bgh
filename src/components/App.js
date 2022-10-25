1080225989e5c7cdadb19a8a905fde4f8ce2373e
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-25 11:39:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-  
+  const [text,setText] = useState('Macro')
+  const [state, setState] = useState(true)
+  const handleClick = () => {
+    if(state === true){
+      setState(false)
+      setText('Polo')
+    }
+    else {
+       setState(true)
+       setText('Macro')
+    }
+  }
   return (
     <div id="main">
-
+      {<h1 id="marco-polo">{state === true ? "Marco": "Polo"}</h1>}
+      <button id="marco-polo-toggler" onClick={handleClick}>{state === true ? 'Polo' : 'Macro'}</button>
     </div>
   )
 }
-
-
 export default App;
